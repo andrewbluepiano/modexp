225f3e540b4ee870afd57cb8928c68da25b2ad0a
Andrew Afonso <andrewbluepiano@hotmail.com>
2019-12-09 13:26:17 -0500

    Create birthdayParadoxProbs.py

---
+++ birthdayParadoxProbs.py
@@ -0,0 +1,33 @@
+# Author: Andrew Afonso
+# Finds probability of no birthday collisions for t people, for single t or range. 
+import sys
+
+def prob(t):
+    # Probability as float
+    p = 0.0
+    # Calculate no collision probability from 1 to t-1
+    for x in range(1, t):
+        if x == 1:
+            p = (1 - (x / 365))
+        else:
+            p = p * (1 - (x / 365))
+    return p
+
+def main():
+    # For single t
+    if len(sys.argv) == 2:
+        #Number of people
+        t = int(sys.argv[1])
+        print("Probability of no Birthday Collision: " + str(prob(t)))
+    # For range of t
+    elif len(sys.argv) == 3:
+        a = int(sys.argv[1])
+        b = int(sys.argv[2])
+        for t in range(a, b+1):
+            print("Probability of no Birthday Collision for " + str(t) + " people: " + str(prob(t)))
+    else:
+        print("Need a number, or range")
+        exit()
+
+if __name__ == "__main__":
+    main()
